aaf85fcd2dfacfce1c85cfaf0602419bc0a7d951
SP6 <qwer14788lll@163.com>
2021-05-19 10:03:45 +0800

    实现了buttonAdd按钮的+1功能

--- app/src/main/java/com/example/gitdemo20/MainActivity.java
+++ app/src/main/java/com/example/gitdemo20/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.gitdemo20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int number = 0;
+    private Button mButtonAdd;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButtonAdd = findViewById(R.id.buttonAdd);
+        mTextView = findViewById(R.id.textView);
+        //给textView赋初值
+        mTextView.setText(String.valueOf(number));
+        //编写buttonAdd点击事件实现+1功能
+        mButtonAdd.setOnClickListener(v -> { mTextView.setText(String.valueOf(++number)); });
     }
 }
